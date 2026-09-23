fc56751f4dd527d63e5988c8926ffabd2a78bc0b
ranjan <ranjansingh3357@gmail.com>
2026-09-23 10:37:00 +0530

    resizable array

--- java/codeFlowEngine/ResizableArray.java
+++ java/codeFlowEngine/ResizableArray.java
@@ -2,6 +2,9 @@ package codeFlowEngine;
 
 public class ResizableArray {
 
+    // We can make them private variables with getter and setter but 
+    // proto-orinted design and builder pattern handles them automatically
+    // more focussed on interview so skipped those
     int capacity = 8;
     int size = 0;
     int[] data = new int[capacity];
@@ -11,9 +14,11 @@ public class ResizableArray {
         if (size == capacity) {
             capacity *= 2;
             int[] temp = new int[capacity];
-            for (int i = 0; i < data.length; i++) {
-                temp[i] = data[i];
-            }
+            // for (int i = 0; i < data.length; i++) {
+            //     temp[i] = data[i];
+            // }
+            /*Optimised version: it leverages memory block copying */
+            System.arraycopy(data, 0, temp, 0, size);
             data = temp;
         }
         data[size] = element;
@@ -21,18 +26,19 @@ public class ResizableArray {
     }
 
     int get(int index) { //— Returns element at index in $O(1)$ time; throws IndexOutOfBoundsException for invalid indices.
-        if (index >= size) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Invalid index for the current array");
         }
         return data[index];
     }
 
     void removeAt(int index) {//— Removes item at index in-place, shifting
-        if (index >= size) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Invalid index for the current array");
         }
         for (int i = index; i + 1 < size; i++) {
             data[i] = data[i+1];
         }
+        size--;
     }
 }
