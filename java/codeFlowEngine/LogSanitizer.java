f2dd113f48d37b65e600c2c7998944e6979e9250
ranjan <ranjansingh3357@gmail.com>
2026-09-25 09:17:01 +0530

    log sanitizer

--- java/codeFlowEngine/LogSanitizer.java
+++ java/codeFlowEngine/LogSanitizer.java
@@ -3,13 +3,13 @@ package codeFlowEngine;
 public class LogSanitizer {
 
     boolean isValidPalindrome(String str) {
-        str = str.toLowerCase();
-        int start = 0, end = str.length() - 1;
+        assert str != null;
+        int start = 0, end = str.length() - 1; // this will be the first line to crash for null, but not after assert
         while (start < end) {
-            if (!Character.isAlphabetic(str.charAt(start))) {start++; continue;}
-            if (!Character.isAlphabetic(str.charAt(end))) {end--; continue;}
+            if (!Character.isLetterOrDigit(str.charAt(start))) {start++; continue;}
+            if (!Character.isLetterOrDigit(str.charAt(end))) {end--; continue;}
 
-            if (str.charAt(start) != str.charAt(end)) {
+            if (Character.toLowerCase(str.charAt(start)) != Character.toLowerCase(str.charAt(end))) {
                 return false;
             }
             start++;end--;
